f67c9e4eb7a6e3af650519c4d70ef1755402e5a3
amirmohammad molazemi <am.molazemi@gmail.com>
2025-12-08 15:39:20 +0330

    refactor: changing the name of function arguments

--- src/composables/useGetProvidersData.js
+++ src/composables/useGetProvidersData.js
@@ -1,7 +1,7 @@
 import { computed } from "vue";
 
-const useGetProvidersData = (getLocaleMessage, locale, date, props) => {
-  const currentMonth = computed(() => (date.month ? date.month - 1 : props.todayDate.month - 1));
+const useGetProvidersData = (getLocaleMessage, locale, date, currentDate) => {
+  const currentMonth = computed(() => (date.month ? date.month - 1 : currentDate.month - 1));
   const weekdays = computed(() => getLocaleMessage(locale.value).weekdays);
   const todayText = computed(() => getLocaleMessage(locale.value).todayText);
   const mainText = computed(() => getLocaleMessage(locale.value).mainText);
